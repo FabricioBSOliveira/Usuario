5d7059f1bfb2a302c3ebd4543cdd793dc28cf136
Fabricio <fabricio.butti@hotmail.com>
2026-01-27 11:20:48 -0300

    Adicionado metodo que salva dados de usuario

--- src/main/java/com/Fabricio/Usuario/infrastructure/repository/EnderecoRepository.java
+++ src/main/java/com/Fabricio/Usuario/infrastructure/repository/EnderecoRepository.java
@@ -1,6 +1,6 @@
 package com.Fabricio.Usuario.infrastructure.repository;
 
-import com.Fabricio.aprendendospring.infrastructure.entity.Endereco;
+import com.Fabricio.Usuario.infrastructure.entity.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EnderecoRepository extends JpaRepository<Endereco,Long> {
